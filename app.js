f3834b1220e7c633dbc6138f64eb5d010bab0446
PrajanBasnet <youraj.basnet991@gmail.com>
2026-02-14 21:57:49 +0545

    feat:message add

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const indexRouter = require("./routers/indexRouter");
 const app = express();
 
 app.set('views',path.join(__dirname,'views'));
+app.use(express.static('public'));
 app.set("view engine","ejs");
 
+app.use(express.urlencoded({extended:true}));
+
 require('dotenv').config();
+
 const PORT = process.env.PORT || 8000;
 
 app.use("/",indexRouter);
+
+app.use((req,res,next)=>{
+    res.status(404).render("error");
+})
 
 app.listen(PORT ,()=>{
     console.log(`Running on Port ${PORT}`);
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,24 +1,43 @@
 const express = require("express");
-
 const indexRouter = express();
 
 const messages = [
     {
-        text:"Hello",
-        user:"prajwal",
+        text: "Hello",
+        user: "prajwal",
         addaded: new Date(),
+        color: "rgb(253,253,202)"
     },
-      {
-        text:"How Do you do",
-        user:"spider",
+    {
+        text: "How Do you do",
+        user: "spider",
         addaded: new Date(),
+        color: "rgb(205,253,204)"
     },
 ]
 
-indexRouter.get("/",(req,res)=>{
-    res.render("index",{messages:messages});
+indexRouter.get("/", (req, res) => {
+    res.render("index", { messages: messages });
 })
 
+indexRouter.post("/", (req, res) => {
+    const newMsg = 
+        {
+            text: req.body.msg,
+            user: req.body.user,
+            addaded: new Date(),
+            color: "rgb(253,253,202)"
+        }
+    
+    console.log(req.body.user);
+    messages.push(newMsg);
 
+    console.log(messages);
+    res.redirect("/");
+})
+
+// indexRouter.get("/:", (req, res) => {
+//     res.send("404")
+// })
 
 module.exports = indexRouter;
